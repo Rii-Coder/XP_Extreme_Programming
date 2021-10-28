f4c8746949f763d1a8461ccaf8d9a89b08316e72
Brandon Hernandez <56750116+MechanicalPuppet@users.noreply.github.com>
2021-10-27 20:43:31 -0700

    Creación de clase curso. Inicialización de la clase grupo. Creación del frame AdminCurso

--- src/main/java/Entities/Curso.java
+++ src/main/java/Entities/Curso.java
@@ -5,10 +5,57 @@
  */
 package Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
- * @author R2
+ * @author Brandon
  */
 public class Curso {
+    
+    String nombre;
+    List<Grupo> grupos; //provisional
+    byte unidades;
+
+    public Curso(){
+        
+    }
+    
+    public Curso(String nombre, ArrayList<Grupo> grupo, byte unidades) {
+        this.nombre = nombre;
+        this.grupos = grupo;
+        this.unidades = unidades;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<Grupo> getGrupos() {
+        return grupos;
+    }
+
+    public void setGrupos(List<Grupo> grupos) {
+        this.grupos = grupos;
+    }
+
+    public byte getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(byte unidades) {
+        this.unidades = unidades;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso:" + nombre;
+    }
+    
     
 }
